f4e01271be2c2031fd931ed912b2b5d80460e7e0
Marco Paco <marco.paco@personiv.com>
2018-03-27 05:14:07 +0800

    Lazy loaded auth components

--- src/router/auth.js
+++ src/router/auth.js
@@ -1,5 +1,7 @@
-import AuthLogin from '@/components/AuthLogin'
 import Auth from '../mixins/Auth'
+const AuthLogin = () => import('../components/AuthLogin')
+const AuthPasswordReset = () => import('../components/AuthPasswordReset')
+const AuthRegister = () => import('../components/AuthRegister')
 
 export default {
   routes: [
@@ -7,17 +9,31 @@ export default {
       path: '/login',
       name: 'Login',
       component: AuthLogin,
-      beforeEnter: (to, from, next) => {
-        if (Auth.methods.getAccessToken()) {
-          return Auth.methods.verifyAccessToken(Auth.methods.getAccessToken(), () => next({ name: 'Home' }))
-        }
-
-        if (to.query.token) {
-          return Auth.methods.verifyAccessToken(to.query.token, () => next({ name: 'Home' }))
-        }
-
-        next()
-      }
+      beforeEnter: (to, from, next) => redirectUserIfAuthenticated(to, from, next)
+    },
+    {
+      path: '/register',
+      name: 'Register',
+      component: AuthRegister,
+      beforeEnter: (to, from, next) => redirectUserIfAuthenticated(to, from, next)
+    },
+    {
+      path: '/password/reset',
+      name: 'PasswordReset',
+      component: AuthPasswordReset,
+      beforeEnter: (to, from, next) => redirectUserIfAuthenticated(to, from, next)
     }
   ]
+}
+
+function redirectUserIfAuthenticated (to, from, next) {
+  if (Auth.methods.getAccessToken()) {
+    return Auth.methods.verifyAccessToken(Auth.methods.getAccessToken(), () => next({ name: 'Home' }))
+  }
+
+  if (to.query.token) {
+    return Auth.methods.verifyAccessToken(to.query.token, () => next({ name: 'Home' }))
+  }
+
+  next()
 }
